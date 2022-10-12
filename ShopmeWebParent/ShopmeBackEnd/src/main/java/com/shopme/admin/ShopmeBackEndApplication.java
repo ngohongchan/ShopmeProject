86fe82cbe3dad370cc149872f8dbb84be54cf71c
Ngo Hong Chan <chan@emobi.biz>
2022-10-12 17:48:49 +0700

    add filter and sort for project

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -1,10 +1,13 @@
 package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @EntityScan({"com.shopme.common.entity", "com.shopme.admin.user"})
 public class ShopmeBackEndApplication {
 
