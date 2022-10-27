b00f789620ac4360f777b9d487eb62feb460859a
Ngo Hong Chan <chan@emobi.biz>
2022-10-27 17:24:29 +0700

    add category form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/CategoryService.java
@@ -1,6 +1,8 @@
 package com.shopme.admin.user;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.transaction.Transactional;
 
@@ -18,6 +20,51 @@ public class CategoryService {
 	
 	public List<Category> listsAll() {
 		return (List<Category>) categoryRepository.findAll();
+	}
+
+	public Category save(Category category) {
+		return categoryRepository.save(category);
+	}
+
+	public List<Category> listCategoriesUsedInForm() {
+		List<Category> categoriesUsedInForm = new ArrayList<>();
+
+		List<Category> categoriesInDB = (List<Category>) categoryRepository.findAll();
+
+		for(Category category : categoriesInDB) {
+			if(category.getParent() == null) {
+				categoriesUsedInForm.add(Category.copyIdAndName(category));
+
+				Set<Category> children = category.getChildren();
+
+				for(Category subCategory : children) {
+					String name = "--" + subCategory.getName();
+					categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
+					listChildren(categoriesUsedInForm, subCategory, 1);
+				}
+			}
+		}
+
+		return categoriesUsedInForm;
+	}
+
+	private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+		int newSubLevel = subLevel + 1;
+		Set<Category> children = parent.getChildren();
+
+		for(Category subCategory : children) {
+			String name = "";
+			for(int i = 0; i < newSubLevel; i++) {
+				name += "--";
+			}
+
+			name += subCategory.getName();
+
+			System.out.println(name);
+			categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
+
+			listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+		}
 	}
 	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/CategoryController.java
@@ -1,32 +1,61 @@
 package com.shopme.admin.user.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import com.shopme.admin.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.shopme.admin.user.CategoryService;
 import com.shopme.common.entity.Category;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CategoryController {
 
-	@Autowired
-	private CategoryService categoryService;
-	
-	@GetMapping("/categories")
-	private String listAll(Model model) {
-		List<Category> listCategories = categoryService.listsAll();
-		model.addAttribute("listCategories", listCategories);
-		
-		return "categories/categories";
-	}
-	
-	@GetMapping("/categories/new")
-	private String newCategory() {
-		return "categories/categories_form";
-	}
-	
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    private String listAll(Model model) {
+        List<Category> listCategories = categoryService.listsAll();
+        model.addAttribute("listCategories", listCategories);
+
+        return "categories/categories";
+    }
+
+    @GetMapping("/categories/new")
+    private String newCategory(Model model) {
+        List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+        model.addAttribute("category", new Category());
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("pageTitle", "Create New Category");
+
+        return "categories/category_form";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory(Category category, RedirectAttributes redirectAttributes,
+                               @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        category.setImage(fileName);
+
+        Category savedCategory = categoryService.save(category);
+        String uploadDir = "../category-images/" + savedCategory.getId();
+        FileUploadUtil.cleanDir(uploadDir);
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+        redirectAttributes.addFlashAttribute("message", "The category has been save successfully.");
+
+        return "redirect:/categories";
+    }
+
 }
