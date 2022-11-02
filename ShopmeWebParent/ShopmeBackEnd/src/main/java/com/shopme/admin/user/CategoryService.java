6dc1829720f447299d8c861ddeb14d0d4307cddc
Ngo Hong Chan <chan@emobi.biz>
2022-11-02 17:59:15 +0700

    add unit test

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/CategoryService.java
@@ -121,6 +121,21 @@ public class CategoryService {
 		if(isCreatingNew) {
 			if(categoryByName != null) {
 				return "DuplicateName";
+			} else {
+				Category categoryByAlias = categoryRepository.findByAlias(alias);
+
+				if(categoryByAlias != null) {
+					return "DuplicateAlias";
+				}
+			}
+		} else {
+			if(categoryByName != null && !categoryByName.getId().equals(id)) {
+				return "DuplicateName";
+			}
+
+			Category categoryByAlias = categoryRepository.findByAlias(alias);
+			if(categoryByAlias != null && !categoryByAlias.getId().equals(id)) {
+				return "DuplicateAlias";
 			}
 		}
 
